94f3e91b3222dac7f16fa4ba77591dc70fe60614
Moksh Jain <97662440+Mokshprojects@users.noreply.github.com>
2024-07-18 14:51:17 +0530

    Update Gallery.py

--- Gallery.py
+++ Gallery.py
@@ -3,9 +3,7 @@ def app():
     st.title("   My Setup   ")
     st.image("images/setup.png")
 
-    st.title("Contact")
-    st.write("For any queries pls email me at:")
-    st.subheader("codermj11@gmail.com")
+   
     st.write("My Github")
 
     st.subheader("https://github.com/Mokshprojects")
